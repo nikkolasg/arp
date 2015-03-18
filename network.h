4f44c2ce3813fede11f9624387b3a79a9ffb958e
nikko <nikko@localhost.localdomain>
2015-03-18 13:10:55 +0000

    before push

--- network.h
+++ network.h
@@ -21,8 +21,28 @@
 
 int get_socketudp();
 struct ifreq get_ifreq(const char * interface);
+<<<<<<< Updated upstream
 int get_mac_address(const char * interface,unsigned char mac[6]);
 struct in_addr get_ip_address(const char * interface);
 int get_mac_addr(char * interface,unsigned char mac[6]);
+=======
+int get_mac_address(const char * interface,struct ether_addr * ether);
+int get_mac_address_old(char * interface,unsigned char mac[6]);
+int get_ip_address(const char * interface,struct in_addr * addr);
+void print_ioctl_error(void);
+
+/* Code from internet. See implementation for details */
+struct route_info
+{
+    struct in_addr dstAddr;
+    struct in_addr srcAddr;
+    struct in_addr gateWay;
+    char ifName[IF_NAMESIZE];
+};
+
+int readNlSock(int sockFd, char *bufPtr, size_t buf_size, unsigned int seqNum, unsigned int pId);
+int parseRoutes(struct nlmsghdr *nlHdr, struct route_info *rtInfo);
+int get_gatewayip(char gatewayip[INET_ADDRSTRLEN]);
+>>>>>>> Stashed changes
 
 #endif
